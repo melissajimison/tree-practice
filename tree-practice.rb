3038324315a06247adafbac6e009a5a3e3ed47e9
melissajimison <melissajimison@gmail.com>
2016-06-25 20:12:15 -0700

    created print_operators method

--- tree-practice.rb
+++ tree-practice.rb
@@ -15,7 +15,49 @@ def print_infix(node)
   print_infix(node.right)
 end
 
-root = TreeNode.new("+")
-root.left = TreeNode.new("3")
-root.right = TreeNode.new("2")
+
+def print_operators(node)
+  return if node == nil
+  if "+-*%/".include? node.value
+    print_operators(node.left)
+    print node.value + " "
+    print_operators(node.right)
+  end
+end
+
+root = TreeNode.new("-")
+root.left = TreeNode.new("+")
+root.right = TreeNode.new("10")
+root.left.right = TreeNode.new("2")
+root.left.left = TreeNode.new("3")
+
 print_infix(root)
+puts
+print_operators(root)
+puts
+puts
+root2 = TreeNode.new("+")
+root2.left = TreeNode.new("*")
+root2.right = TreeNode.new("2")
+root2.left.right = TreeNode.new("4")
+root2.left.left = TreeNode.new("3")
+
+print_infix(root2)
+puts
+print_operators(root2)
+
+puts
+puts
+root3 = TreeNode.new("-")
+root3.left = TreeNode.new("*")
+root3.left.right = TreeNode.new("+")
+root3.left.left = TreeNode.new("4")
+root3.left.right.left = TreeNode.new("3")
+root3.left.right.right = TreeNode.new("2")
+root3.right = TreeNode.new("%")
+root3.right.left = TreeNode.new("10")
+root3.right.right = TreeNode.new("5")
+
+print_operators(root3)
+puts
+print_infix(root3)
